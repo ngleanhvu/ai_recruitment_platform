aad30045f5a471f5b2802ea1db060c1179727acd
Nguyen Le Anh Vu <vunguyen.0208200402@gmail.com>
2026-08-18 10:07:40 +0700

    feat(candidate-service): add upload resume api

---
+++ candidate-service/candidate-service-application/src/main/java/com/ngleanhvu/candidate/application/port/input/resume/UploadResumeUseCase.java
@@ -0,0 +1,8 @@
+package com.ngleanhvu.candidate.application.port.input.resume;
+
+import com.ngleanhvu.candidate.domain.candidate.CandidateId;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface UploadResumeUseCase {
+    void execute(CandidateId candidateId, MultipartFile file);
+}
